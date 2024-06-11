ccaba0006d7b2985f7bfff6dd2f9ff73d971e2c1
KIJI2610 <xxxalexey52@gmail.com>
2024-06-11 15:29:59 +0500

    exp modif

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ function BuyBonus(bonus){
         case '+1':
             if(countNum >= price.clickPlus1){
                 countNum -= price.clickPlus1; count.textContent = countNum
+                localStorage.setItem('countNum', countNum)
                 UpCount++; localStorage.setItem('UpCount', UpCount);
                 price.clickPlus1 = price.clickPlus1 * 2; localStorage.setItem('clickPlus1', price.clickPlus1)
                 clicck1_price.textContent = price.clickPlus1
@@ -42,7 +43,8 @@ function BuyBonus(bonus){
 
         case '+1auto':
             if(countNum >= price.clickAuto1){
-                countNum -= price.clickAuto1; count.clickAuto1 = countNum
+                countNum -= price.clickAuto1; count.textContent = countNum
+                localStorage.setItem('countNum', countNum)
                 UpCountAuto++; localStorage.setItem('UpCountAuto', UpCountAuto);
                 price.clickAuto1 = price.clickAuto1 * 2; localStorage.setItem('clickAuto1', price.clickAuto1)
                 auto1_price.textContent = price.clickAuto1
@@ -57,6 +59,7 @@ function BuyBonus(bonus){
             case '+2':
             if(countNum >= price.clickPlus2){
                 countNum -= price.clickPlus2; count.textContent = countNum
+                localStorage.setItem('countNum', countNum)
                 UpCount += 2; localStorage.setItem('UpCount', UpCount);
                 price.clickPlus2 = price.clickPlus2 * 2; localStorage.setItem('clickPlus2', price.clickPlus2)
                 clicck2_price.textContent = price.clickPlus2
@@ -70,6 +73,7 @@ function BuyBonus(bonus){
         case '+2auto':
             if(countNum >= price.clickAuto2){
                 countNum -= price.clickAuto2; count.textContent = countNum
+                localStorage.setItem('countNum', countNum)
                 UpCountAuto += 2; localStorage.setItem('UpCountAuto', UpCountAuto);
                 price.clickAuto2 = price.clickAuto2 * 2; localStorage.setItem('clickAuto2', price.clickAuto2)
                 auto2_price.textContent = price.clickAuto2
@@ -85,6 +89,7 @@ function BuyBonus(bonus){
             case '+3':
             if(countNum >= price.clickPlus3){
                 countNum -= price.clickPlus3; count.textContent = countNum
+                localStorage.setItem('countNum', countNum)
                 UpCount++; localStorage.setItem('UpCount', UpCount);
                 price.clickPlus3 = price.clickPlus3 * 2; localStorage.setItem('clickPlus3', price.clickPlus3)
                 clicck3_price.textContent = price.clickPlus3
@@ -97,7 +102,8 @@ function BuyBonus(bonus){
 
         case '+3auto':
             if(countNum >= price.clickAuto3){
-                countNum -= price.clickAuto3; count.clickAuto3 = countNum
+                countNum -= price.clickAuto3; count.textContent = countNum
+                localStorage.setItem('countNum', countNum)
                 UpCountAuto += 3; localStorage.setItem('UpCountAuto', UpCountAuto);
                 price.clickAuto3 = price.clickAuto3 * 2; localStorage.setItem('clickAuto3', price.clickAuto3)
                 auto3_price.textContent = price.clickAuto3
@@ -112,7 +118,7 @@ function BuyBonus(bonus){
     }
 }
 
-
+let lvl = (localStorage.getItem('lvl') === null)?(0):(localStorage.getItem('lvl'))
 function experienceUp(){
-    
+
 }
