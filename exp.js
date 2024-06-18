32f3f2c12782c7cac82be270fa339ad1f9e5968d
KIJI2610 <xxxalexey52@gmail.com>
2024-06-18 16:42:15 +0500

    new save 180624 1642

--- exp.js
+++ exp.js
@@ -3,18 +3,23 @@ const lvl = document.getElementById('lvl')
 const exp_num = document.getElementById('exp-num')
 let experience_num = (localStorage.getItem('experience_num') === null)?(0):(Number(localStorage.getItem('experience_num')))
 let start_game_lvl = (localStorage.getItem('start_game_lvl') === null)?(0):(Number(localStorage.getItem('start_game_lvl')))
-let current_lvl_max_exp = (localStorage.getItem('current_lvl_max_exp') === null)?(10):(Number(localStorage.getItem('current_lvl_max_exp')))
+let current_lvl_max_exp = (localStorage.getItem('current_lvl_max_exp') === null)?(100):(Number(localStorage.getItem('current_lvl_max_exp')))
 let len_shkala_visual = (localStorage.getItem('len_shkala_visual') === null)?(0):(Number(localStorage.getItem('len_shkala_visual')))
-
 
 center_bar.addEventListener('click', () => {
     experience_num++
+
     exp_num.textContent = experience_num
     if(experience_num % 5 === 0){
         setTimeout(() => {
             localStorage.setItem('experience_num', experience_num)
         }, 0)
     }
+
+    // if(experience_num === current_lvl_max_exp){
+    //     exp.style.width = `${}%`
+    // }
+
     
     if(current_lvl_max_exp === experience_num){
         start_game_lvl++; localStorage.setItem('start_game_lvl', start_game_lvl); lvl.textContent = start_game_lvl
